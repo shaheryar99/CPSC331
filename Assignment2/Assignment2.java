02012ee4316b6ed3b5ffeba5f5731a418639887d
Shaheryar Syed <63892413+shaheryar99@users.noreply.github.com>
2024-02-29 01:04:49 -0700

    Add files via upload

---
+++ Assignment2/Assignment2.java
@@ -0,0 +1,99 @@
+/**
+* Assignment 2 - Shaheryar Syed 
+* CPSC 331 
+* UCID: 30052162
+* TA: Harnoor Khehra 
+*/
+
+ import java.util.Arrays;
+
+public class Assignment2 {
+
+    /* Exercise 2 - Step 1 */
+    public static int[] exercise2_1(int[] arr) {
+
+        int[] tmparr = new int[arr.length];
+
+        for(int i = 0; i < arr.length; i++) {
+            tmparr[i] = arr[arr.length-1-i];
+        }
+        return tmparr;
+    }
+
+    /* Exercise 2 - Step 2 */
+
+    public static int[] exercise2_2(int[] arr) {
+        
+        int[] tmparr = new int[arr.length];
+
+        for(int i = 0; i < arr.length; i++) {
+            if (i == arr.length-1) {
+                tmparr[i] = arr[i];
+            }
+            else if(arr[i] == 0 || arr[i+1] == 0) {
+                tmparr[i] = 0;
+            }
+            else if(arr[i] < arr[i+1]) {
+                tmparr[i] = arr[i+1] / arr[i];
+            }
+            else {
+                tmparr[i] = arr[i] / arr[i+1];
+            }
+        }
+
+        return tmparr;
+    }
+
+    /* Exercise 3 - Step 3 */
+
+    public static int[] exercise2_3(int[] arr) {
+
+        int[] tmparr = new int[arr.length];
+        int sortedIndex = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        tmparr[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i-1]) {
+                tmparr[sortedIndex++] = arr[i];
+            }
+        }
+
+        arr = new int[sortedIndex];
+
+        for (int i = 0; i < sortedIndex; i++) {
+            arr[i] = tmparr[i];
+        }
+
+        return arr;
+    }
+    public static void main(String[] args) {
+
+        // Initial Array
+        int[] ex2_array = {3,15,0,18,4,10,2,5,25};
+
+        // Method for Exercise 2 Step 1
+        ex2_array = exercise2_1(ex2_array);
+
+        // Method for Exercise 2 Step 2
+        ex2_array = exercise2_2(ex2_array);
+
+        // Method for Exercise 2 Step 3
+        ex2_array = exercise2_3(ex2_array);
+
+        // Print the final array
+        for (int i = 0; i < ex2_array.length; i++) {
+            System.out.println(ex2_array[i]);
+        }
+
+    }
+}
