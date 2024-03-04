8fcda3967ec2fdfc6906e1fbfe5a62bc3415e91f
ShaheryarS <shaheryar.syed@ucalgary.ca>
2024-03-03 21:56:50 -0700

    Update to ascending order

--- Assignment2/Assignment2.java
+++ Assignment2/Assignment2.java
@@ -54,7 +54,7 @@ public class Assignment2 {
        // Sort the Array in descending order 
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
+                if (arr[i] < arr[j]) {
                     int temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
